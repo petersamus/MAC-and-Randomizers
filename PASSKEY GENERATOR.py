16c4caec9db5cd28b1887a918761280e2bcfc2ef
petersamus <70871628+petersamus@users.noreply.github.com>
2022-07-09 07:14:41 -0300

    Update PASSKEY GENERATOR.py

--- PASSKEY GENERATOR.py
+++ PASSKEY GENERATOR.py
@@ -3,7 +3,7 @@
 
 import random
 
-str_length = random.randint(8, 16)
+str_length = random.randint(8, 20)
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
